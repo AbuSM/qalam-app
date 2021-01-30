77856032266ca878c31e5846dec327a79b8ae8f3
Fattoh <android17cpu@gmail.com>
2021-01-31 03:09:17 +0500

    Fix structure, add cors, ajax

--- api/app.py
+++ api/app.py
@@ -2,13 +2,15 @@ import flask
 import sqlite3
 import os
 import os.path
-from flask import request, jsonify
+from flask import request, jsonify, render_template
+from flask_cors import CORS, cross_origin
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "requests.db")
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 def dict_factory(cursor, row):
     d = {}
@@ -18,7 +20,7 @@ def dict_factory(cursor, row):
 
 API = '/api/v1/{}'
 
-@app.route(API.format('request'), methods=['POST'])
+@app.route(API.format('requests'), methods=['POST', 'OPTIONS'])
 def main():
 	print("Got request: ", request.form)
 	name = request.form.get('name')
@@ -29,9 +31,10 @@ def main():
 	conn = sqlite3.connect(db_path)
 	conn.row_factory = dict_factory
 	cur = conn.cursor()
-	print(12312)
+
 	cur.execute(f"INSERT INTO requests(name, phone, email, message) values('{name}', '{phone}', '{email}', '{message}')")
 	conn.commit()
-	return 'Success'
+	return jsonify(dict({"message": "ok"}))
 
-app.run()
+if __name__ == '__main__':
+	app.run()
